99fde2f7449514dbc2a9f969cf131bb790ee7294
Dave Boucher <dave@boucherfamily.com>
2019-11-28 16:04:49 -0700

    Adds page generation and basic item formatting.

---
+++ DirectoryApp/DirectoryApp/Models/DirectoryEntry.cs
@@ -0,0 +1,22 @@
+﻿namespace DirectoryApp
+{
+    public class DirectoryEntry
+    {
+        public int ID { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public string Address1 { get; set; }
+        public string Address2 { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Zipcode { get; set; }
+        public string Children { get; set; }
+        public string PhoneWork { get; set; }
+        public string PhoneHome { get; set; }
+        public string PhoneCell { get; set; }
+        public string Email { get; set; }
+        public string Email2 { get; set; }
+        public string Email3 { get; set; }
+        public string Picture { get; set; }
+    }
+}
--- DirectoryApp/DirectoryApp/Utilities/DataAccess.cs
+++ DirectoryApp/DirectoryApp/Utilities/DataAccess.cs
@@ -1,9 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Data;
 using System.Data.OleDb;
+using System.Linq;
 
 namespace DirectoryApp
 {
@@ -13,11 +11,10 @@ namespace DirectoryApp
         {
             try
             {
-                using (var connection = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;"
-                    + "Data Source=" + Properties.Settings.Default.DatabaseFilePath + ";"
-                    + "Persist Security Info=False;"))
+                using (var connection = GetConnection())
                 {
                     connection.Open();
+                    connection.Close();
                 }
             } 
             catch
@@ -25,6 +22,62 @@ namespace DirectoryApp
                 return false;
             }
             return true;
+        }
+
+
+        public static List<DirectoryEntry> GetDirectoryEntries()
+        {
+            var directoryEntries = new List<DirectoryEntry>();
+
+            using (var connection = GetConnection())
+            {
+                // Set query
+                var sql = "SELECT ID, LastName, FirstName, Address1, Address2, City, State, Zipcode, "
+                    + "Children, PhoneWork, PhoneHome, PhoneCell, Email, Email2, Email3, Picture "
+                    + "FROM Directory ORDER BY LastName, FirstName";
+
+                // Pull data
+                var command = new OleDbDataAdapter(sql, connection);
+                connection.Open();
+                var dataSet = new DataSet();
+                command.Fill(dataSet, "Directory");
+
+                // Convert to a strongly typed list of model objects
+                directoryEntries =
+                      (from row in dataSet.Tables["Directory"].AsEnumerable()
+                       select new DirectoryEntry
+                       {
+                           ID = row.Field<int>("ID"),
+                           LastName = row.Field<string>("LastName"),
+                           FirstName = row.Field<string>("FirstName"),
+                           Address1 = row.Field<string>("Address1"),
+                           Address2 = row.Field<string>("Address2"),
+                           City = row.Field<string>("City"),
+                           State = row.Field<string>("State"),
+                           Zipcode = row.Field<string>("Zipcode"),
+                           Children = row.Field<string>("Children"),
+                           PhoneWork = row.Field<string>("PhoneWork"),
+                           PhoneHome = row.Field<string>("PhoneHome"),
+                           PhoneCell = row.Field<string>("PhoneCell"),
+                           Email = row.Field<string>("Email"),
+                           Email2 = row.Field<string>("Email2"),
+                           Email3 = row.Field<string>("Email3"),
+                           Picture = row.Field<string>("Picture")
+                       }).ToList();
+
+                connection.Close();
+            }
+
+            // Return the data
+            return directoryEntries;
+        }
+
+
+        private static OleDbConnection GetConnection()
+        {
+            return new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;"
+                    + "Data Source=" + Properties.Settings.Default.DatabaseFilePath + ";"
+                    + "Persist Security Info=False;");
         }
     }
 }
