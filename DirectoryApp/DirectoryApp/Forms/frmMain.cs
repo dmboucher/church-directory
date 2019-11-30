915c43618e42067685e4829597451fbe839c424d
Dave Boucher <dave@boucherfamily.com>
2019-11-30 15:56:02 -0700

    Split everything out into separate forms. Wired up the main grid.

--- DirectoryApp/DirectoryApp/Forms/frmMain.cs
+++ DirectoryApp/DirectoryApp/Forms/frmMain.cs
@@ -1,63 +1,39 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
 using System.Windows.Forms;
 
 namespace DirectoryApp
 {
     public partial class FrmMain : Form
     {
-        public Common Common { get; set; }
+        public List<DirectoryEntry> DirectoryEntries { get; set; }
 
         public FrmMain()
         {
             InitializeComponent();
-            Common = new Common(this);
-            Common.SetDatabaseFilePath(Properties.Settings.Default.DatabaseFilePath);
-            Common.SetOutputFolder(Properties.Settings.Default.OutputFolder);
+            DirectoryEntries = DataAccess.GetDirectoryEntries();
+            var bindingList = new BindingList<DirectoryEntry>(DirectoryEntries);
+            var source = new BindingSource(bindingList, null);
+            dgvDirectoryEntries.DataSource = source;
+            dgvDirectoryEntries.Columns["ID"].ReadOnly = true;
         }
 
-        private void txtDatabaseFilePath_Click(object sender, EventArgs e)
+        private void mnuSettings_Click(object sender, EventArgs e)
         {
-            var result = diaDatabaseFilePath.ShowDialog();
-            if (result == DialogResult.OK)
-            {
-                Common.SetDatabaseFilePath(diaDatabaseFilePath.FileName);
-            }
+            FrmSettings settingsForm = new FrmSettings();
+            settingsForm.Show();
         }
 
-        private void btnClearDatabaseFilePath_Click(object sender, EventArgs e)
+        private void mnuPrint_Click(object sender, EventArgs e)
         {
-            Common.SetDatabaseFilePath(string.Empty);
+            FrmPrint printForm = new FrmPrint(DirectoryEntries);
+            printForm.Show();
         }
 
-        private void btnTestDatabaseConnection_Click(object sender, EventArgs e)
+        private void dgvDirectoryEntries_CellValueChanged(object sender, DataGridViewCellEventArgs e)
         {
-            if (DataAccess.CheckDatabaseConnection())
-            {
-                MessageBox.Show("Connection to database is successful!", "Success!", MessageBoxButtons.OK, MessageBoxIcon.Information);
-            }
-            else
-            {
-                MessageBox.Show("Unable to connect to database.", "Failed", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }
-        }
-
-        private void btnGenerateDirectoryPages_Click(object sender, EventArgs e)
-        {
-            OfficeAutomation.GenerateDirectoryPages(btnGenerateDirectoryPages, lblGenerateDirectoryStatus);
-        }
-
-        private void txtOutputFolder_Click(object sender, EventArgs e)
-        {
-            var result = diaOutputFolder.ShowDialog();
-            if (result == DialogResult.OK)
-            {
-                Common.SetOutputFolder(diaOutputFolder.SelectedPath);
-            }
-        }
-
-        private void btnClearOutputFolder_Click(object sender, EventArgs e)
-        {
-            Common.SetOutputFolder(string.Empty);
+            MessageBox.Show("something changed");
         }
     }
 }
---
+++ DirectoryApp/DirectoryApp/Forms/frmSettings.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace DirectoryApp
+{
+    public partial class FrmSettings : Form
+    {
+        public FrmSettings()
+        {
+            InitializeComponent();
+            SetDatabaseFilePath(Properties.Settings.Default.DatabaseFilePath);
+            SetPhotosFolder(Properties.Settings.Default.PhotosFolder);
+            SetOutputFolder(Properties.Settings.Default.OutputFolder);
+        }
+
+        private void txtDatabaseFilePath_Click(object sender, EventArgs e)
+        {
+            var result = diaDatabaseFilePath.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                SetDatabaseFilePath(diaDatabaseFilePath.FileName);
+            }
+        }
+
+
+        private void txtPhotosFolder_Click(object sender, EventArgs e)
+        {
+            var result = diaPhotosFolder.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                SetPhotosFolder(diaPhotosFolder.SelectedPath);
+            }
+        }
+
+
+        private void txtOutputFolder_Click(object sender, EventArgs e)
+        {
+            var result = diaOutputFolder.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                SetOutputFolder(diaOutputFolder.SelectedPath);
+            }
+        }
+
+
+        private void btnTestDatabaseConnection_Click(object sender, EventArgs e)
+        {
+            if (DataAccess.CheckDatabaseConnection())
+            {
+                MessageBox.Show("Connection to database is successful!", "Success!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("Unable to connect to database.", "Failed", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+
+        private void btnClearDatabaseFilePath_Click(object sender, EventArgs e)
+        {
+            SetDatabaseFilePath(string.Empty);
+        }
+
+
+        private void btnClearPhotosFolder_Click(object sender, EventArgs e)
+        {
+            SetPhotosFolder(string.Empty);
+        }
+
+
+        private void btnClearOutputFolder_Click(object sender, EventArgs e)
+        {
+            SetOutputFolder(string.Empty);
+        }
+
+
+        public void SetDatabaseFilePath(string filePath)
+        {
+            txtDatabaseFilePath.Text = filePath;
+            diaDatabaseFilePath.FileName = filePath;
+            Properties.Settings.Default.DatabaseFilePath = filePath;
+            Properties.Settings.Default.Save();
+            btnTestDatabaseConnection.Enabled = !string.IsNullOrWhiteSpace(filePath);
+        }
+
+
+        public void SetPhotosFolder(string folderPath)
+        {
+            txtPhotosFolder.Text = folderPath;
+            diaPhotosFolder.SelectedPath = folderPath;
+            Properties.Settings.Default.PhotosFolder = folderPath;
+            Properties.Settings.Default.Save();
+        }
+
+
+        public void SetOutputFolder(string folderPath)
+        {
+            txtOutputFolder.Text = folderPath;
+            diaOutputFolder.SelectedPath = folderPath;
+            Properties.Settings.Default.OutputFolder = folderPath;
+            Properties.Settings.Default.Save();
+        }
+    }
+}
